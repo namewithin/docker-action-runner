d431fc4435e007a303b4601e2e3a19703b14b9ea
leonid korniienko <korn2001@gmail.com>
2020-07-25 21:52:24 +0300

    add labels.

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM ubuntu:18.04
 ARG RUNNER_VERSION="2.267.1"
 RUN useradd -m docker
-RUN apt update -y && apt install -y curl jq build-essential libssl-dev libffi-dev python3 python3-venv python3-dev
+RUN apt-get update -qq -y && apt-get install -qq -y curl jq build-essential libssl-dev libffi-dev python3 python3-venv python3-dev
 RUN mkdir /home/docker/actions-runner && cd /home/docker/actions-runner \
     && curl -O -L https://github.com/actions/runner/releases/download/v${RUNNER_VERSION}/actions-runner-linux-x64-${RUNNER_VERSION}.tar.gz \
     && tar xzf ./actions-runner-linux-x64-${RUNNER_VERSION}.tar.gz
--- start.sh
+++ start.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
-REPO=$REPO
-ACCESS_TOKEN=$ACCESS_TOKEN
-
 REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/repos/${REPO}/actions/runners/registration-token | jq .token --raw-output)
-
+CONFIG_OPTIONS="--token ${REG_TOKEN}"
 # shellcheck disable=SC2164
-cd /home/docker/actions-runner
 
+if [[ -n $RUNNER_LABELS ]]; then
+    CONFIG_OPTIONS="${CONFIG_OPTIONS} --labels ${RUNNER_LABELS}"
+fi
+
+
+cd /home/docker/actions-runner
 echo "Registering runner..."
-./config.sh --url https://github.com/${REPO} --token ${REG_TOKEN}
+./config.sh --url https://github.com/${REPO} ${CONFIG_OPTIONS}
 
 cleanup() {
-    echo "Removing runner..."
-    ./config.sh remove --unattended --token ${REG_TOKEN}
+  echo "Removing runner..."
+  ./config.sh remove --unattended --token ${REG_TOKEN}
 }
 
 trap 'cleanup; exit 130' INT
 trap 'cleanup; exit 143' TERM
 
-./run.sh & wait $!
+./run.sh &
+wait $!

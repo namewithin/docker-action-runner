38611fd5ab9a8ddf75da7cba6bd09b5e6d66cfe6
leonid korniienko <leo@pdffiller.com>
2021-09-20 10:24:20 +0300

    back to runsvc

--- start.sh
+++ start.sh
@@ -39,4 +39,5 @@ cleanup() {
 trap cleanup SIGINT SIGQUIT SIGTERM TERM INT QUIT
 
 echo "running service"
-./bin/Runner.Listener run --startuptype service
+#./bin/Runner.Listener run --startuptype service
+./bin/runsvc.sh
